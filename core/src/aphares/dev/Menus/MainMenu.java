aca6c66255ded620880bf560ea97321829ae34bc
aphares <aphares.dev@gmail.com>
2017-11-13 05:38:11 +0000

    Added Menus

---
+++ core/src/aphares/dev/Menus/MainMenu.java
@@ -0,0 +1,63 @@
+package aphares.dev;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenu implements Screen {
+    final Game game;
+    OrthographicCamera camera;
+
+    public MainMenu(final Game gam) {
+        game = gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.setColor(0,0,0,1);
+        game.font.draw(game.batch, "Infinity Maze", 200, 300);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
